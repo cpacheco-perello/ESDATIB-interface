978b65e6be4a8b0c571ee62ef5c322a626f97e7d
cpacheco-perello <cpacheco.perello@outlook.com>
2026-02-05 13:01:41 +0100

    search page

---
+++ Patch_Interface/agents/consumer/atro-config/astro/SearchTabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-tabs">
+    <STabs :tabs="tabs">
+      <template #quick-search>
+        <QuickSearch :search="search" />
+      </template>
+      <template #advanced-search>
+        <AdvancedSearch />
+      </template>
+    </STabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { STabs } from '@simpl/vue-components';
+import AdvancedSearch from './AdvancedSearch.vue';
+import QuickSearch from './QuickSearch.vue';
+
+defineProps<{
+  search: string;
+}>();
+
+const tabs = [
+  { tabName: 'quick-search', tabTitle: 'Cerca ràpida' },
+  { tabName: 'advanced-search', tabTitle: 'Cerca avançada' },
+];
+</script>
