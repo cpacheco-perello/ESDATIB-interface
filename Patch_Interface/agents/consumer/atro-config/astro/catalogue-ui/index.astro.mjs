2e5447c1888bbbbdc181f9df0921a868cbdf822b
cpacheco-perello <cpacheco.perello@outlook.com>
2026-02-10 16:00:49 +0100

    corregido indez astro.mjs

--- Patch_Interface/agents/consumer/atro-config/astro/catalogue-ui/index.astro.mjs
+++ Patch_Interface/agents/consumer/atro-config/astro/catalogue-ui/index.astro.mjs
@@ -1,8 +1,8 @@
-import { e as createComponent, f as createAstro, k as renderComponent, r as renderTemplate } from '../chunks/astro/server_CXNiU3AQ.mjs';
+import { e as createComponent, f as createAstro, k as renderComponent, r as renderTemplate } from '../chunks/astro/server_Cnh9Zfda.mjs';
 import 'piccolore';
-import { P as PageHeader } from '../chunks/PageHeader_C3QRZHPG.mjs';
-import { $ as $$Layout } from '../chunks/Layout_DHJhrArt.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_BlV96EW8.mjs';
+import { P as PageHeader } from '../chunks/PageHeader_bAco-vn0.mjs';
+import { $ as $$Layout } from '../chunks/Layout_CeKZ73gq.mjs';
+export { r as renderers } from '../chunks/_@astro-renderers_DM40fJWi.mjs';
 
 const $$Astro = createAstro();
 const $$Index = createComponent(($$result, $$props, $$slots) => {
